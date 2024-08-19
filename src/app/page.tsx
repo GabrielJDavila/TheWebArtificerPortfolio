2bedc8b65587690eb1e9be55e8996188051502a9
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-08-18 20:00:25 -0500

    Added about.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
-import HomeUi from "./ui/home";
+import HomeUi from "./ui/homepageui/home";
+import AboutUi from "./ui/homepageui/about";
 export default function Page() {
   return (
-    <main className="flex justify-center items-center text-center py-24">
+    <main className="flex flex-col justify-center items-center text-center gap-20 py-24">
       <HomeUi />
+      <AboutUi />
     </main>
   );
 }
