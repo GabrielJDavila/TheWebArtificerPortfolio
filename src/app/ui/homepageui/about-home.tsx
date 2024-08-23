e428579db03d1652452952b5d1e225be2670a0e2
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-08-22 19:56:41 -0500

    Added more Tailwind Styles
    Added more effects to projects display image, on hover image will zoom in without increasing container size, and all links are functional. Also made an edit to about description for home page.

--- src/app/ui/homepageui/about-home.tsx
+++ src/app/ui/homepageui/about-home.tsx
@@ -5,7 +5,7 @@ import LogoColorMode from "../logocolormode"
 import { useColorTheme } from "../../lib/ColorModeContext"
 import Link from "next/link"
 
-const inter = Inter({ subsets: ["latin"], weight: "200" });
+const inter = Inter({ subsets: ["latin"], weight: "200" })
 const mont = Montserrat({subsets: ["latin"], weight: "200"})
 
 export default function AboutHomeUi() {
@@ -21,8 +21,8 @@ export default function AboutHomeUi() {
                     alt="picture of Gabriel Davila, wearing a black collared shirt against a beige background."
                     className="flex flex-col w-1/2 lg:w-1/4 text-center justify-center items-center m-auto rounded-3xl rotate-3"
                 />
-                <p className={`${mont.className} dark:text-white`}>I'm Gabriel, and I'm not your traditional developer. I have 7+ years in the fitness industry as a coach, developing excellent customer service and soft skills.
-                    While I love coaching, I have a natural drive to create and build things. So in 2019 I learned to code, and that turned from a hobby into a bachelors of Computer Science.
+                <p className={`${mont.className} dark:text-white`}>I'm Gabriel, and I'm not your traditional developer. I have 7+ years in the fitness industry, developing excellent customer service and soft skills.
+                    In 2019 I learned how to code, and fell in love with the creative and problem-solving nature of web & software development.
                     I now use my unique skillset to create stunning websites and helpful applications for small businesses, all while providing an unparalleled customer experience.</p>
                 <Link href="#" className="bg-green-600 w-24 m-auto p-2 rounded-full hover:bg-sky-600">Read more</Link>
             </div>
--- src/app/ui/homepageui/projects-home.tsx
+++ src/app/ui/homepageui/projects-home.tsx
@@ -1,12 +1,7 @@
 import Link from "next/link"
+import { Montserrat } from "next/font/google"
 
-const projectsArr = [
-    "100onthehill.png",
-    "frenchmasteryblueprint.png",
-    "mgcleanpros.png",
-    "thefitbook.png"
-]
-
+const mont = Montserrat({subsets: ["latin"], weight: "200"})
 
 export default function ProjectsHomeUi() {
     return (
@@ -18,51 +13,66 @@ export default function ProjectsHomeUi() {
                     href="https://100onthehill.com/"
                     target="_blank"
                 >
-                <div className="relative group">
-                    <img
-                        src="100onthehill.png"
-                        alt="screenshot of 100 On The Hill website homepage."
-                        className="w-full h-full max-w-md"
-                    />
-                    <div className="absolute transition-all ease-in-out duration-300 bottom-0 sm:h-auto w-full md:h-0 md:group-hover:h-16 dark:bg-slate-700 py-2 md:py-0 md:group-hover:py-2 md:overflow-hidden">
-                        <h3>One Hundred On The Hill</h3>
-                        <p>Charity Fitness Event</p>
+                    <div className="relative group overflow-hidden">
+                        <img
+                            src="100onthehill.png"
+                            alt="screenshot of 100 On The Hill website homepage."
+                            className="w-full h-full max-w-md object-cover transition-transform duration-300 ease-in-out transform hover:scale-110"
+                        />
+                        <div className="absolute transition-all ease-in-out duration-300 bottom-0 sm:h-auto w-full md:h-0 md:group-hover:h-16 dark:bg-slate-700 py-2 md:py-0 md:group-hover:py-2 md:overflow-hidden">
+                            <h3 className={`${mont.className}`}>One Hundred On The Hill</h3>
+                            <p className={`${mont.className}`}>Charity Fitness Event</p>
+                        </div>
                     </div>
-                </div>
                 </Link>
-                <div className="relative group">
-                    <img
-                        src="frenchmasteryblueprint.png"
-                        alt="screenshot of French Mastery Blueprint website homepage."
-                        className="w-full max-w-md"
-                    />
-                    <div className="absolute transition-all ease-in-out duration-300 bottom-0 sm:h-auto w-full md:h-0 md:group-hover:h-16 dark:bg-slate-700 py-2 md:py-0 md:group-hover:py-2 md:overflow-hidden">
-                        <h3>French Mastery Blueprint</h3>
-                        <p>Online French Tutoring</p>
+                <Link
+                    href="https://frenchmasteryblueprint.com/"
+                    target="_blank"
+                >
+                    <div className="relative group overflow-hidden">
+                        <img
+                            src="frenchmasteryblueprint.png"
+                            alt="screenshot of French Mastery Blueprint website homepage."
+                            className="w-full max-w-md object-cover transition-transform duration-300 ease-in-out transform hover:scale-110"
+                        />
+                        <div className="absolute transition-all ease-in-out duration-300 bottom-0 sm:h-auto w-full md:h-0 md:group-hover:h-16 dark:bg-slate-700 py-2 md:py-0 md:group-hover:py-2 md:overflow-hidden">
+                            <h3 className={`${mont.className}`}>French Mastery Blueprint</h3>
+                            <p className={`${mont.className}`}>Online French Tutoring</p>
+                        </div>
                     </div>
-                </div>
-                <div className="relative group">
-                    <img
-                        src="mgcleanpros.png"
-                        alt="screenshot of M&G Clean Pros website homepage."
-                        className="w-full max-w-md"
-                    />
-                    <div className="absolute transition-all ease-in-out duration-300 bottom-0 sm:h-auto w-full md:h-0 md:group-hover:h-16 dark:bg-slate-700 py-2 md:py-0 md:group-hover:py-2 md:overflow-hidden">
-                        <h3>M&G Clean Pros</h3>
-                        <p>Cleaning Services</p>
+                </Link>
+                <Link
+                    href="https://mgcleanpros.com/"
+                    target="_blank"
+                >
+                    <div className="relative group overflow-hidden">
+                        <img
+                            src="mgcleanpros.png"
+                            alt="screenshot of M&G Clean Pros website homepage."
+                            className="w-full max-w-md object-cover transition-transform duration-300 ease-in-out transform hover:scale-110"
+                        />
+                        <div className="absolute transition-all ease-in-out duration-300 bottom-0 sm:h-auto w-full md:h-0 md:group-hover:h-16 dark:bg-slate-700 py-2 md:py-0 md:group-hover:py-2 md:overflow-hidden">
+                            <h3 className={`${mont.className}`}>M&G Clean Pros</h3>
+                            <p className={`${mont.className}`}>Cleaning Services</p>
+                        </div>
                     </div>
-                </div>
-                <div className="relative group">
-                    <img
-                        src="thefitbook.png"
-                        alt="screenshot of The Fitbook dashboard page."
-                        className="w-full max-w-md"
-                    />
-                    <div className="absolute transition-all ease-in-out duration-300 bottom-0 sm:h-auto w-full md:h-0 md:group-hover:h-16 dark:bg-slate-700 py-2 md:py-0 md:group-hover:py-2 md:overflow-hidden">
-                        <h3>The Fitbook</h3>
-                        <p>Fitness Journal Web Application</p>
+                </Link>
+                <Link
+                    href="https://thefitbook.netlify.app/"
+                    target="_blank"
+                >
+                    <div className="relative group overflow-hidden">
+                        <img
+                            src="thefitbook.png"
+                            alt="screenshot of The Fitbook dashboard page."
+                            className="w-full max-w-md object-cover transition-transform duration-300 ease-in-out transform hover:scale-110"
+                        />
+                        <div className="absolute transition-all ease-in-out duration-300 bottom-0 sm:h-auto w-full md:h-0 md:group-hover:h-16 dark:bg-slate-700 py-2 md:py-0 md:group-hover:py-2 md:overflow-hidden">
+                            <h3 className={`${mont.className}`}>The Fitbook</h3>
+                            <p className={`${mont.className}`}>Fitness Web App</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
             </div>
         </section>
     )
