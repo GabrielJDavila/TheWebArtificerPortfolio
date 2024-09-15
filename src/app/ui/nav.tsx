ec984cf3e55594f480f3e15df14f1c3aafa59489
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-09-15 15:16:19 -0500

    Update nav.tsx
    Nav now works properly. Services drop down menu work correctly, and refs work correctly. Style changes to reflect that.

--- src/app/ui/nav.tsx
+++ src/app/ui/nav.tsx
@@ -13,7 +13,8 @@ import Link from "next/link"
 export default function Nav() {
     const {openNav, setOpenNav} = useNav()
     const {openServicesNav, setOpenServicesNav} = useServicesNav()
-    const menuRef = useRef<HTMLElement | null>(null)
+    const menuRef = useRef<HTMLDivElement | null>(null)
+    const servicesMenuRef = useRef<SVGSVGElement | null>(null)
     const {colorTheme, setColorTheme} = useColorTheme()
     const [lightMode, setLightMode] = useState(false)
     const [windowWidth, setWindowWidth] = useState(window.innerWidth)
@@ -55,23 +56,35 @@ export default function Nav() {
     }
 
     const handleClickOutside = (e: MouseEvent) => {
-        if(menuRef.current && !menuRef.current.contains(e.target as Node)) {
+        const target = e.target as Node
+
+        if(
+            menuRef.current &&
+            !menuRef.current.contains(target) &&
+            servicesMenuRef.current &&
+            !servicesMenuRef.current.contains(target)
+        ) {
+            console.log(e.target)
             setOpenNav(false)
             console.log("clicked outside")
         }
     }
 
+    function handleLinkClick() {
+        setOpenNav(false)
+        setOpenServicesNav(false)
+    }
+
     return (
         <div className="flex h-full flex-col md:px-2 ml-auto">
             <div className="fixed z-40 w-full flex justify-center items-center font-mono p-2 md:pt-2 md:pb-0 md:px-0">
-                <div className="z-index-100 flex h-full flex-row items-center md:px-2 ml-auto md:hidden shadow-lg bg-white dark:bg-zinc-800 border-gray-200 dark:border-gray-700 border-2 px-4 py-2 rounded-full gap-2 mr-2">
+                <div ref={menuRef} className="z-index-100 flex h-full flex-row items-center md:px-2 ml-auto md:hidden shadow-lg bg-white dark:bg-zinc-800 border-gray-200 dark:border-gray-700 border-2 px-4 py-2 rounded-full gap-2 mr-2">
                     <p>Menu</p>
                     <ChevronDownIcon onClick={handleClick} className={`w-4 text-black rounded md:hidden dark:text-white transition-all ${openNav ? "rotate-180" : "rotate-0"}`}/>
                 </div>
                 {windowWidth < 768 && <LightDarkMode />}
                 {!lightMode ?
                 <nav
-                    
                     className={`fixed z-40 top-2 right-0 left-0 z-50 ${openNav ? "dark:border-gray-700" : "border-none"} ${openNav ? "dark:border-2" : "border-none"} 
                     dark:bg-zinc-800 flex grow flex-col justify-between text-left gap-4 ${openNav ? "max-h-lg" : "h-0"} transition-all space-x-0 space-y-2 
                     ${openNav? "p-8" : "p-0"} w-11/12 max-w-2xl m-auto rounded-3xl ${grenze.className}
@@ -83,19 +96,19 @@ export default function Nav() {
                         <p className="text-gray-200">Menu</p>
                         <XMarkIcon onClick={handleClick} className="w-6 text-gray-200"/>
                     </div>
-                    <Link href="/" className="text-white cursor-pointer hover:underline hover:underline-offset-8 border-b-2 border-gray-700 pb-2 text-lg">Home</Link>
-                    <Link href="/#projects-home" className="text-white cursor-pointer hover:underline hover:underline-offset-8 border-b-2 border-gray-700 pb-2 text-lg">Projects</Link>
-                    <Link href="/about" className="text-white cursor-pointer hover:underline hover:underline-offset-8 border-b-2 border-gray-700 pb-2 text-lg">About</Link>
+                    <Link onClick={handleLinkClick} href="/" className="text-white cursor-pointer hover:underline hover:underline-offset-8 border-b-2 border-gray-700 pb-2 text-lg">Home</Link>
+                    <Link onClick={handleLinkClick} href="/#projects-home" className="text-white cursor-pointer hover:underline hover:underline-offset-8 border-b-2 border-gray-700 pb-2 text-lg">Projects</Link>
+                    <Link onClick={handleLinkClick} href="/about" className="text-white cursor-pointer hover:underline hover:underline-offset-8 border-b-2 border-gray-700 pb-2 text-lg">About</Link>
                     <div className="flex flex-col gap-4 items-center justify-left text-left mr-auto border-b-2 border-gray-700 pb-2">
                         <div className="flex flex-row justify-left items-center text-left gap-4 mr-auto">
                             <Link href="/services" className="text-white cursor-pointer text-lg">Services</Link>
-                            <ChevronDownIcon onClick={handleServicesClick} className={`w-4 text-black rounded md:hidden dark:text-white transition-all ${openServicesNav ? "rotate-180" : "rotate-0"}`}/>
+                            <ChevronDownIcon ref={servicesMenuRef} onClick={handleServicesClick} className={`w-4 text-black rounded md:hidden dark:text-white transition-all ${openServicesNav ? "rotate-180" : "rotate-0"}`}/>
                         </div>
                         {openServicesNav &&
                         <div className="flex flex-col justify-left items-center text-left gap-2 mr-auto">
-                            <Link href="/services/service-one" className="text-left mr-auto">Custom Software</Link>
-                            <Link href="/services/service-two" className="text-left mr-auto">Web Development</Link>
-                            <Link href="/services/service-three" className="text-left mr-auto">Website Management & Hosting</Link>
+                            <Link onClick={handleLinkClick} href="/services/service-one" className="text-left mr-auto cursor-pointer hover:underline hover:underline-offset-8">Custom Software</Link>
+                            <Link onClick={handleLinkClick} href="/services/service-two" className="text-left mr-auto cursor-pointer hover:underline hover:underline-offset-8">Web Development</Link>
+                            <Link onClick={handleLinkClick} href="/services/service-three" className="text-left mr-auto cursor-pointer hover:underline hover:underline-offset-8">Website Management & Hosting</Link>
                         </div>
                         }
                     </div>
@@ -120,7 +133,7 @@ export default function Nav() {
                 :
                 <nav
                     className={`fixed top-2 right-0 left-0 z-50 ${openNav ? "border-gray-200" : "border-none"} ${openNav ? "border-2" : "border-none"} 
-                    bg-white flex grow flex-col justify-between text-left gap-4 ${openNav ? "h-96" : "h-0"} transition-all space-x-0 space-y-2 
+                    bg-white flex grow flex-col justify-between text-left gap-4 ${openNav ? "max-h-lg" : "h-0"} transition-all space-x-0 space-y-2 
                     ${openNav? "p-8" : "p-0"} w-11/12 max-w-2xl m-auto rounded-3xl ${grenze.className}
                     md:flex-row md:h-auto md:justify-center md:text-center`}
                 >
@@ -130,11 +143,23 @@ export default function Nav() {
                         <p className="text-zinc-600">Menu</p>
                         <XMarkIcon className="w-6 text-zinc-600"/>
                     </div>
-                    <Link href="/" className="text-black cursor-pointer border-b-2 border-gray-300 pb-2 text-lg hover:underline hover:underline-offset-8">Home</Link>
-                    <Link href="/#projects-home" className="text-black cursor-pointer border-b-2 border-gray-300 pb-2 text-lg hover:underline hover:underline-offset-8">Projects</Link>
-                    <Link href="/about" className="text-black cursor-pointer border-b-2 border-gray-300 pb-2 text-lg hover:underline hover:underline-offset-8">About</Link>
-                    <Link href="/services" className="text-black cursor-pointer border-b-2 border-gray-300 pb-2 text-lg hover:underline hover:underline-offset-8">Services</Link>
-                    <Link href="/contact" className="text-black cursor-pointer text-lg">Contact</Link>
+                    <Link onClick={handleLinkClick} href="/" className="text-black cursor-pointer border-b-2 border-gray-300 pb-2 text-lg hover:underline hover:underline-offset-8">Home</Link>
+                    <Link onClick={handleLinkClick} href="/#projects-home" className="text-black cursor-pointer border-b-2 border-gray-300 pb-2 text-lg hover:underline hover:underline-offset-8">Projects</Link>
+                    <Link onClick={handleLinkClick} href="/about" className="text-black cursor-pointer border-b-2 border-gray-300 pb-2 text-lg hover:underline hover:underline-offset-8">About</Link>
+                    <div className="flex flex-col gap-4 items-center justify-left text-left mr-auto border-b-2 border-gray-300 pb-2">
+                        <div className="flex flex-row justify-left items-center text-left gap-4 mr-auto">
+                            <Link href="/services" className="text-black cursor-pointer text-lg">Services</Link>
+                            <ChevronDownIcon ref={servicesMenuRef} onClick={handleServicesClick} className={`w-4 text-black rounded md:hidden dark:text-white transition-all ${openServicesNav ? "rotate-180" : "rotate-0"}`}/>
+                        </div>
+                        {openServicesNav &&
+                        <div className="flex flex-col justify-left items-center text-left gap-2 mr-auto">
+                            <Link onClick={handleLinkClick} href="/services/service-one" className="text-left mr-auto cursor-pointer hover:underline hover:underline-offset-8">Custom Software</Link>
+                            <Link onClick={handleLinkClick} href="/services/service-two" className="text-left mr-auto cursor-pointer hover:underline hover:underline-offset-8">Web Development</Link>
+                            <Link onClick={handleLinkClick} href="/services/service-three" className="text-left mr-auto cursor-pointer hover:underline hover:underline-offset-8">Website Management & Hosting</Link>
+                        </div>
+                        }
+                    </div>
+                    <Link onClick={handleLinkClick} href="/contact" className="text-black cursor-pointer text-lg">Contact</Link>
                     </>
                     }
                     {windowWidth >= 768 &&
