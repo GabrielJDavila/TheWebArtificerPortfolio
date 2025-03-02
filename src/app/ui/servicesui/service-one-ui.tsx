294539dff655dd64aefd5839e0520aee3980d1c3
Gabriel Davila <gdavila0819@gmail.com>
2025-03-02 17:45:10 -0600

    Deleted window width tracking on all servicesui pages, using tailwind responsive breakpoints instead.

--- src/app/ui/servicesui/service-one-ui.tsx
+++ src/app/ui/servicesui/service-one-ui.tsx
@@ -5,16 +5,6 @@ import { Montserrat } from "next/font/google"
 const mont = Montserrat({subsets: ["latin"], weight: ["200", "300"]})
 
 export default function ServiceOneUi() {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-
-    useEffect(() => {
-        if(typeof window !== "undefined") {
-            const handleResize = () => setWindowWidth(window.innerWidth)
-            window.addEventListener("resize", handleResize)
-
-            return () => window.removeEventListener("resize", handleResize)
-        }
-    }, [])
 
     return (
         <div className="w-full md:max-w-4xl bg-zinc-800 py-20 md:pt-24 px-8">
@@ -24,8 +14,8 @@ export default function ServiceOneUi() {
                     <h1 className="text-3xl text-white">Custom Software</h1>
                 </div>
             </div>
-            {windowWidth < 1024 ?
-            <div className="w-full flex flex-col justify-center items-center m-auto">
+            
+            <div className="w-full flex flex-col justify-center items-center m-auto lg:hidden">
                 <h2 className={`${mont.className} text-2xl text-left md:w-3/4 lg:w-1/2 text-white`}>Why a Business Needs Software</h2>
                 <br></br>
                 <img src="/softwareimg.png" className="w-full md:w-3/4 lg:w-1/2" alt="image of coding interface background."/>
@@ -42,8 +32,8 @@ export default function ServiceOneUi() {
                 </div>
                 <Link href="/contact" className="w-20 text-center text-white mt-8 bg-sky-500 hover:bg-sky-700 p-2 rounded-full">Contact</Link>
             </div>
-            :
-            <div className="w-full flex flex-row justify-center items-center m-auto gap-12">
+            
+            <div className="hidden lg:flex w-full flex-row justify-center items-center m-auto gap-12">
                 <div className="md:w-3/4 lg:w-1/2 flex flex-col justify-center gap-4 text-white">
                     <h2 className={`${mont.className} text-2xl text-left md:w-3/4 lg:w-full text-white`}>Why a Business Needs Software</h2>
                     <p className={`${mont.className}`}>As a business owner/manager, you know that there are so many things to do in a day, and only so much time.</p>
@@ -58,36 +48,7 @@ export default function ServiceOneUi() {
                 </div>
                 <img src="/softwareimg.png" className="w-full md:w-3/4 lg:w-1/2" alt="image of coding interface background."/>
             </div>
-            }
+            
         </div>
     )
 }
-
-// import Link from "next/link"
-// import { Montserrat } from "next/font/google"
-// const mont = Montserrat({subsets: ["latin"], weight: ["200", "300"]})
-
-// export default function ServiceOneUi() {
-//     return (
-//         <div className="w-full md:max-w-5xl bg-zinc-800 p-8">
-//             {/* <div>
-//                 <img src="/WA_icon_v2.1.png" className="w-20"/>
-//                 <h1>Custom Software</h1>
-//             </div> */}
-
-//             <div>
-//                 <h2 className={`${mont.className} text-2xl`}>Why a Business Needs Software</h2>
-//                 <br></br>
-//                 <p className={`${mont.className}`}>As a business owner/manager, you know that there are so many things to do in a day, and only so much time.</p>
-//                 <br></br>
-//                 <p className={`${mont.className}`}>There are many minute tasks that suck up your time - time that could be better used to serve customers or build the business.</p>
-//                 <br></br>
-//                 <p className={`${mont.className}`}>What if you could automate or streamline certain business tasks? That is where software comes in. Custom software that is tailored
-//                     to your business needs can increase productivity, decrease costs, and allow you the time to focus on what you do best - running
-//                     the business!
-//                 </p>
-                
-//             </div>
-//         </div>
-//     )
-// }
--- src/app/ui/servicesui/service-two-ui.tsx
+++ src/app/ui/servicesui/service-two-ui.tsx
@@ -22,8 +22,8 @@ export default function ServiceTwoUi() {
                     {/* <img src="/WA_icon_v2.1.png" className="w-48 h-48 text-center m-auto"/> */}
                     <h1 className="text-3xl">Web Development</h1>
             </div>
-            {windowWidth < 1024 ?
-            <div className="w-full flex flex-col justify-center items-center m-auto text-white">
+            
+            <div className="w-full flex flex-col justify-center items-center m-auto text-white lg:hidden">
                 <h2 className={`${mont.className} text-2xl text-left md:w-3/4 lg:w-1/2`}>A Well Designed and Performative Website</h2>
                 <br></br>
                 <img src="/webdesignimg.png" className="w-full md:w-3/4 lg:w-1/2" alt="image of website on various devices; laptop, tablet, phone."/>
@@ -41,8 +41,8 @@ export default function ServiceTwoUi() {
                 </div>
                 <Link href="/contact" className="w-20 text-center mt-8 bg-sky-500 hover:bg-sky-700 p-2 rounded-full">Contact</Link>
             </div>
-            :
-            <div className="w-full flex flex-row justify-center items-center m-auto gap-12">
+            
+            <div className="hidden lg:flex flex-row w-full justify-center items-center m-auto gap-12">
                 <div className="md:w-3/4 lg:w-1/2 flex flex-col justify-center gap-4">
                     <h2 className={`${mont.className} text-2xl text-left md:w-3/4 lg:w-full`}>A Well Designed and Performative Website</h2>
                     <p className={`${mont.className}`}>A website is the digital face of your business. It is what your potential customers see and interact with,
@@ -58,34 +58,7 @@ export default function ServiceTwoUi() {
                 </div>
                 <img src="/webdesignimg.png" className="w-full md:w-3/4 lg:w-1/2" alt="image of website on various devices; laptop, tablet, phone."/>
             </div>
-            }
+            
         </div>
     )
 }
-
-// import Link from "next/link"
-// import { Montserrat } from "next/font/google"
-// const mont = Montserrat({subsets: ["latin"], weight: ["200", "300"]})
-
-// export default function ServiceTwoUi() {
-//     return (
-//             <div className="w-full md:max-w-5xl bg-zinc-800 p-8">
-//                 <div className="md:w-3/4 m-auto">
-//                     <h2 className={`${mont.className} text-2xl`}>A Well Designed and Performative Website</h2>
-//                     <br></br>
-//                     <p className={`${mont.className}`}>A website is the digital face of your business. It is what your potential customers see and interact with,
-//                         so it is important to have a well designed site that users love to use and conveys your business mission properly.
-//                     </p>
-//                     <br></br>
-//                     <p className={`${mont.className}`}>It starts with interviewing you - the owner/manager - to better understand your business, story, goals, and ideal customers.</p>
-//                     <br></br>
-//                     <p className={`${mont.className}`}>From there, I will design and develop a website that will meet your needs. I have checkpoints where I will check in with you to go
-//                         over key features. If you already have designs in mind, I can work with that as well! Already have a website, but need to have it freshened up? I work with
-//                         clients on updates and re-designs as well.
-//                     </p>
-//                     <br></br>
-//                     <p className={`${mont.className}`}>Contact me for a quote!</p>
-//                 </div>
-//             </div>
-//     )
-// }
